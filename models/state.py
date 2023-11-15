e672074af65b6dcd097c6c551e9e68348ceeafa2
Frandy G Slueue <frandy4ever@gmail.com>
2023-11-14 18:57:15 -0600

    update some files

--- models/state.py
+++ models/state.py
@@ -3,7 +3,7 @@
 import models
 from models.base_model import BaseModel, Base
 from models.city import City
-from os import getenv
+import os
 import sqlalchemy
 from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
@@ -11,10 +11,21 @@ from sqlalchemy.orm import relationship
 
 class State(BaseModel, Base):
     """Representation of state """
+    __tablename__ = 'states'  # Define __tablename__ outside the conditional block
+
     if models.storage_t == "db":
-        __tablename__ = 'states'
         name = Column(String(128), nullable=False)
         cities = relationship("City", backref="state")
     else:
         name = ""
-	cites = []
+        cities = []
+
+
+def cities(self):
+    """Getter method to return list of City objects"""
+    if os.getenv('HBNB_TYPE_STORAGE') != 'db':
+        city_list = []
+        for city in models.storage.all(City).values():
+            if city.state_id == self.id:
+                city_list.append(city)
+        return city_list
