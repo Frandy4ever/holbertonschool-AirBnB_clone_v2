4a24f5449489a6a5049ce9df0580eb01a3c90f21
Frandy G Slueue <frandy4ever@gmail.com>
2023-11-14 20:13:36 -0600

    update 8-cities files

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -9,17 +9,17 @@ from models.state import State
 app = Flask(__name__)
 
 
-@app.teardown_appcontext
-def close_session(exception):
-    """After each request you must remove the current SQLAlchemy Session"""
-    storage.close()
-
-
 @app.route('/cities_by_states', strict_slashes=False)
 def cities_by_states():
     """Display a HTML page with a list of all State objects and their cities"""
     states = storage.all(State)
     return render_template('8-cities_by_states.html', states=states)
+
+
+@app.teardown_appcontext
+def close_session(exception):
+    """After each request you must remove the current SQLAlchemy Session"""
+    storage.close()
 
 
 if __name__ == '__main__':
