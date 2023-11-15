41694a2da539b5ae354b1d88fc8d9111a590a454
Frandy G Slueue <frandy4ever@gmail.com>
2023-11-14 21:11:56 -0600

    update 9-states.py

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -5,7 +5,6 @@ Script that starts a Flask web application
 from flask import Flask, render_template
 from models import storage
 from models.state import State
-from models.city import City
 
 app = Flask(__name__)
 
@@ -13,22 +12,21 @@ app = Flask(__name__)
 @app.route('/states', strict_slashes=False)
 def states():
     """Display a HTML page with a list of all State objects"""
-    states = storage.all(State).values()
+    states = storage.all('State')
     return render_template('9-states.html', state=states)
 
 
 @app.route('/states/<id>', strict_slashes=False)
-def state_cities(id):
+def state_id(id):
     """Display a HTML page with cities of a specific State"""
-    state = storage.get(State, id)
-    if state:
-        return render_template('state_cities.html', state=state)
-    else:
-        return render_template('not_found.html')
+    for state in storage.all('State').values():
+        if state.id == id:
+            return render_template('9-states.html', state=states)
+    return render_template('9-states.html')
 
 
 @app.teardown_appcontext
-def close_session(exception):
+def teardown(exception):
     """After each request you must remove the current SQLAlchemy Session"""
     storage.close()
 
